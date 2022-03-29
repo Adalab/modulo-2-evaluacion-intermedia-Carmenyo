9bb7226daabcb5ba597e50404510b40c1e7bc181
Carmenyo <carmencastroyo@gmail.com>
2022-03-29 10:53:00 +0200

    styles

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 //const declaration
-const selectedValue = document.querySelector(".js-select");
+const select = document.querySelector(".js-select");
 const BetValue = document.querySelector ("js-number")
 const submitBtn = document.querySelector (".js-submit");
 const indicationText = document.querySelector(".js-indication");
@@ -9,17 +9,19 @@ const playerValue = document.querySelector(".js-player");
 const randomNumber = getRandomNumber(6);
 
 console.log('El número aleatorio es:', randomNumber);
-
-
+//
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
   }
-  let randomNumber = getRandomNumber(6);
 
 
-function playGame() {
-
+function playGame(event) {
+    event.preventDefault();
+    const selectedValue = parseInt(select.value);
     if (randomNumber === selectedValue ) { indicationText.innerHTML = "Has ganado el doble de lo apostado";
     } else {
     indicationText.innerHTML = "Has perdido lo apostado";
     }
+  }
+
+submitBtn.addEventListener('click', playGame)
