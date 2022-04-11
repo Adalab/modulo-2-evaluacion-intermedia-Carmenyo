4ca5dfaaa88cd5adeb2586bfa3e25128a767a18b
Carmenyo <98713741+Carmenyo@users.noreply.github.com>
2022-04-11 15:25:13 +0200

    update game

--- main.js
+++ main.js
@@ -1,27 +1,77 @@
 "use strict";
 //const declaration
-const select = document.querySelector(".js-select");
-const BetValue = document.querySelector ("js-number")
-const submitBtn = document.querySelector (".js-submit");
-const indicationText = document.querySelector(".js-indication");
-const playerValue = document.querySelector(".js-player");
+const selectElement = document.querySelector(".js-selected");
+const btn = document.querySelector(".js-play");
+const result = document.querySelector(".js-result");
+const saldo = document.querySelector(".js-saldo");
+const cantidad = document.querySelector(".js-cantidad");
+const textMoney = document.querySelector(".js-money");
+const reinit = document.querySelector(".js-reload");
 
-const randomNumber = getRandomNumber(6);
+let salValue = 50;
 
-console.log('El número aleatorio es:', randomNumber);
-//
+textMoney.innerHTML = salValue;
+
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-  }
+  return Math.ceil(Math.random() * max);
+}
+
+function game(selectValue, randomNumber) {
+  //agregamos el min y el max al input que recoge la apuesta
+
+  let valueInput = valorMaximoInput();
 
 
-function playGame(event) {
-    event.preventDefault();
-    const selectedValue = parseInt(select.value);
-    if (randomNumber === selectedValue ) { indicationText.innerHTML = "Has ganado el doble de lo apostado";
+  if (valueInput > salValue) {
+    result.innerHTML = "No puedes apostar mas de lo que tienes";
+    reinit.classList.remove("hidden");
+    btn.classList.add("hidden");
+  } else {
+    if (selectValue === randomNumber) {
+      result.innerHTML = "Has ganado el doble de lo apostado";
+      salValue += valueInput * 2;
+      if (salValue >= 200) {
+        result.innerHTML = "No puedes seguir jugando ya has ganado lo máximo";
+        salValue = 200;
+        reinit.classList.remove("hidden");
+        btn.classList.add("hidden");
+      }
     } else {
-    indicationText.innerHTML = "Has perdido lo apostado";
+      result.innerHTML = "Has perdido lo apostado";
+      salValue = salValue - valueInput;
     }
+
+    if (salValue === 0) {
+      result.innerHTML = "No tienes saldo para continuar";
+      reinit.classList.remove("hidden");
+      btn.classList.add("hidden");
+    }
+    textMoney.innerHTML = salValue;
   }
 
-submitBtn.addEventListener('click', playGame)
+  function valorMaximoInput() {
+    cantidad.setAttribute("min", 0);
+    cantidad.setAttribute("max", salValue);
+    let valueInput = parseInt(cantidad.value);
+    return valueInput;
+  }
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  const selectValue = parseInt(selectElement.value);
+  const randomNumber = getRandomNumber(6);
+  game(selectValue, randomNumber);
+}
+
+// Creamos una función que define el valor máximo y mínimo del input de la apuesta
+
+function handleClickReinit() {
+  cantidad.value = "";
+  salValue = 50;
+  reinit.classList.add("hidden");
+  btn.classList.remove("hidden");
+}
+
+btn.addEventListener("click", handleClick);
+reinit.addEventListener("click", handleClickReinit);
